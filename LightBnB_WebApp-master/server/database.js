37906d702985fcfbd6d91fb1815aef7ef85a5744
Rafay Siddiqui <rafays.siddiqui@gmail.com>
2021-08-20 06:24:11 +0000

    Refactor database.js to use SQL for logging in and creating new users

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -17,18 +17,25 @@ const pool = new Pool({
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
+
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+  return pool
+    .query(`
+  SELECT * FROM users WHERE email = $1;
+  `, [email])
+    .then((result) => {
+      if (result.rows.length !== 0) {
+      return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
+
 exports.getUserWithEmail = getUserWithEmail;
 
 /**
@@ -37,7 +44,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`
+  SELECT * FROM users WHERE id = $1;
+  `, [id])
+    .then((result) => {
+      if (result.rows.length !== 0) {
+      return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.getUserWithId = getUserWithId;
 
@@ -47,11 +67,22 @@ exports.getUserWithId = getUserWithId;
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`
+  INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *;
+  `, [user.name, user.email, user.password])
+    .then((result) => {
+      if (result.rows.length !== 0) {
+      return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
 exports.addUser = addUser;
 
@@ -77,7 +108,7 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = (options, limit = 10) => {
   return pool
-  .query(`SELECT * FROM properties LIMIT $1;`, [limit])
+    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
     .then((result) => {
       console.log(result.rows);
     })
